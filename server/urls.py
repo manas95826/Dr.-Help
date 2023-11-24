e9549f4646b68121ea2c7c09396746d3c0161538
harshraj2717 <harshraj2717@gmail.com>
2023-11-24 15:06:04 +0530

    update settings and gitignore

--- server/urls.py
+++ server/urls.py
@@ -1,7 +1,7 @@
 """server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('authenticator.urls')),
 ]
