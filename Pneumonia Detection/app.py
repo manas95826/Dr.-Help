9aa153183358f3831f2ef29b86dcc8af44758e0c
Manas Chopra <74074241+manas95826@users.noreply.github.com>
2023-11-24 16:33:16 +0530

    Update app.py

--- Pneumonia Detection/app.py
+++ Pneumonia Detection/app.py
@@ -1,17 +1,16 @@
-from keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
-import os
-import streamlit as st
+from keras.models import load_model
 
-model = load_model("keras_model.h5", compile=False)
+def classify_pneumonia(image_path):
+    model = load_model("keras_model.h5", compile=False)
+    class_names = ["0", "1"]
 
-class_names = ["0", "1"]  # Make sure the class names match the order in your model
-
-def classify_image(img):
+    img = Image.open(image_path)
+    
     # Check if the image is in RGB format
     if img.mode != "L":
-        return "Invalid"
+        return "Please upload the X-ray file only", None
 
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     img = img.convert('RGB')  # Convert to RGB mode to ensure 3 channels
@@ -31,32 +30,9 @@ def classify_image(img):
     confidence_score = model.predict(data)
     predicted_class = np.argmax(confidence_score)
 
-    return predicted_class
-
-def main():
-    st.title("Image Classification")
-    st.header("Pneumonia X-Ray Classification")
-    st.text("Upload a Pneumonia X-Ray for classification")
-
-    folder = st.text_input("Enter folder path containing images:", type="default")
-    
-    if folder and os.path.exists(folder):
-        image_files = [f for f in os.listdir(folder) if f.lower().endswith(('.jpg', '.jpeg'))]
-        
-        for image_file in image_files:
-            image_path = os.path.join(folder, image_file)
-            img = Image.open(image_path)
-            predicted_class = classify_image(img)
-            
-            st.write(f"Image: {image_file}")
-            if predicted_class == 1:
-                st.success("Pneumonia found!")
-            else:
-                st.error("No Pneumonia Found.")
-            
-            st.image(img, caption=f"Classified as: {class_names[predicted_class]}", use_column_width=True)
+    if predicted_class == 1:
+        return "Pneumonia found!", "success"
     else:
-        st.warning("Please provide a valid folder path")
+        return "No Pneumonia Found.", "error"
 
-if __name__ == "__main__":
-    main()
+
