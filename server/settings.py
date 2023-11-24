eb196c7cc29e17de9a0badc4401352bd90ac0132
Jatin Dua <jdua004@gmail.com>
2023-11-24 16:53:19 +0530

    feat: Add pdf file upload

--- server/settings.py
+++ server/settings.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'authenticator'
+    'authenticator',
+    'core',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+
+CORS_ORIGIN_ALLOW_ALL = True
--- server/urls.py
+++ server/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authenticator.urls')),
-]
+    path('', include('core.urls')),
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
