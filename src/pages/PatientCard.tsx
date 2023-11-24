290cfde0867341f7ec852b979319e5e381d2a95e
Manas Chopra <74074241+manas95826@users.noreply.github.com>
2023-11-25 01:33:00 +0530

    Update PatientCard.tsx

--- src/pages/PatientCard.tsx
+++ src/pages/PatientCard.tsx
@@ -1,44 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PatientCard from './PatientCard';
+import React from 'react'
 
-const Patient: React.FC = () => {
-  const handlePrediction = () => {
-    console.log('Predicting for this patient...');
-    // Redirect to localhost:5000
-    window.location.href = 'http://localhost:8501';
-  };
-
-  const buttonStyles: React.CSSProperties = {
-    backgroundColor: '#4CAF50',
-    border: 'none',
-    color: 'white',
-    padding: '15px 32px',
-    textAlign: 'center',
-    textDecoration: 'none',
-    display: 'inline-block',
-    fontSize: '16px',
-    margin: '4px 2px',
-    cursor: 'pointer',
-    borderRadius: '8px',
-    transition: 'background-color 0.3s',
-    animation: 'pulse 1.5s infinite',
-  };
-
+const PatientCard = (props: any) => {
   return (
-    <div className='flex flex-1 justify-center items-center gap-48'>
-      <div>
-        <img src="/assets/defaultpfp.jpg" alt="" height={300} width={300} className='rounded-3xl'/>
-      </div>
-      <Link to="/" className='absolute top-3/4 left-64 border border-black p-2 rounded-xl'>Go back</Link>
-      <PatientCard first="lorem" second="insanity" third="could've been better" fourth="lmao"/>
+    <div className='w-1/2 h-2/3 border border-black bg-slate-200 rounded-xl'>
+<p className='font-bold m-4'>Recent Medical Report</p>
 
-      {/* Animated button for predicting */}
-      <button style={buttonStyles} onClick={handlePrediction}>
-        Predict for this patient
-      </button>
+
+<p className='ml-5 mt-2'>1. {props.first}</p>
+<p className='ml-5 mt-2'>2. {props.second}</p>
+
+<p className='ml-5 mt-2'>3. {props.third}</p>
+
+<p className='ml-5 mt-2'>4. {props.fourth}</p>
+
+
     </div>
-  );
-};
+  )
+}
 
-export default Patient;
+export default PatientCard
